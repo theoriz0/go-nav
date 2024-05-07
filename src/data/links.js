ce2515f7fd62e4c39f800cc5a7e0b2c5367818e2
theoriz0 <zhang_00710@qq.com>
2024-05-07 09:19:26 +0800

    replace example index page with nav

--- src/data/links.js
+++ src/data/links.js
@@ -1,41 +1,101 @@
-export const people = [{
-    id: 0,
-    name: 'Creola Katherine Johnson',
-    profession: 'mathematician',
-  }, {
-    id: 1,
-    name: 'Mario José Molina-Pasquel Henríquez',
-    profession: 'chemist',
-  }, {
-    id: 2,
-    name: 'Mohammad Abdus Salam',
-    profession: 'physicist',
-  }, {
-    id: 3,
-    name: 'Percy Lavon Julian',
-    profession: 'chemist',  
-  }, {
-    id: 4,
-    name: 'Subrahmanyan Chandrasekhar',
-    profession: 'astrophysicist',
-  }, {
-    id: 5,
-    name: 'Subrahmanyan Chandrasekhar',
-    profession: 'astrophysicist',
-  }, {
-    id: 6,
-    name: 'Subrahmanyan Chandrasekhar',
-    profession: 'astrophysicist',
-  }, {
-    id: 7,
-    name: 'Subrahmanyan Chandrasekhar',
-    profession: 'astrophysicist',
-  }, {
-    id: 8,
-    name: 'Subrahmanyan Chandrasekhar',
-    profession: 'astrophysicist',
-  }, {
-    id: 9,
-    name: 'Subrahmanyan Chandrasekhar',
-    profession: 'astrophysicist',
-  }];
+export const topLinks = [{
+  id: 0,
+  title: 'Creola Katherine Johnson',
+  link: 'mathematician',
+  desc: '',
+}, {
+  id: 1,
+  title: 'Mario José Molina-Pasquel Henríquez',
+  link: 'chemist',
+  desc: '',
+}, {
+  id: 2,
+  title: 'Mohammad Abdus Salam',
+  link: 'physicist',
+  desc: '',
+}, {
+  id: 3,
+  title: 'Percy Lavon Julian',
+  link: 'chemist',
+}, {
+  id: 4,
+  title: 'Subrahmanyan Chandrasekhar',
+  link: 'astrophysicist',
+  desc: '',
+}, {
+  id: 5,
+  title: 'Subrahmanyan Chandrasekhar',
+  link: 'astrophysicist',
+  desc: '',
+}, {
+  id: 6,
+  title: 'Subrahmanyan Chandrasekhar',
+  link: 'astrophysicist',
+  desc: '',
+}, {
+  id: 7,
+  title: 'Subrahmanyan Chandrasekhar',
+  link: 'astrophysicist',
+  desc: '',
+}, {
+  id: 8,
+  title: 'Subrahmanyan Chandrasekhar',
+  link: 'astrophysicist',
+  desc: '',
+}, {
+  id: 9,
+  title: 'Subrahmanyan Chandrasekhar',
+  link: 'astrophysicist',
+  desc: '',
+}];
+
+export const mainLinks = [{
+  id: 0,
+  title: 'Creola Katherine Johnson',
+  link: 'mathematician',
+  desc: '',
+}, {
+  id: 1,
+  title: 'Mario José Molina-Pasquel Henríquez',
+  link: 'chemist',
+  desc: '',
+}, {
+  id: 2,
+  title: 'Mohammad Abdus Salam',
+  link: 'physicist',
+  desc: '',
+}, {
+  id: 3,
+  title: 'Percy Lavon Julian',
+  link: 'chemist',
+}, {
+  id: 4,
+  title: 'Subrahmanyan Chandrasekhar',
+  link: 'astrophysicist',
+  desc: '',
+}, {
+  id: 5,
+  title: 'Subrahmanyan Chandrasekhar',
+  link: 'astrophysicist',
+  desc: '',
+}, {
+  id: 6,
+  title: 'Subrahmanyan Chandrasekhar',
+  link: 'astrophysicist',
+  desc: '',
+}, {
+  id: 7,
+  title: 'Subrahmanyan Chandrasekhar',
+  link: 'astrophysicist',
+  desc: '',
+}, {
+  id: 8,
+  title: 'Subrahmanyan Chandrasekhar',
+  link: 'astrophysicist',
+  desc: '',
+}, {
+  id: 9,
+  title: 'Subrahmanyan Chandrasekhar',
+  link: 'astrophysicist',
+  desc: '',
+}];
